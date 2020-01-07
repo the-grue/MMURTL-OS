225899c1afb2a3892257864dd322a4c91d52ca46
James T. Sprinkle <the-grue@hotmail.com>
2020-01-07 13:55:58 -0800

    Modified "note to self".

--- gcc/tools/rundump.c
+++ gcc/tools/rundump.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
 
 	if(argc < 2)
 	{
-		printf("Must provide filename dumbass!\n");
+		printf("Must provide filename as parameter!\n");
 		return 1;
 	}
 	fp = fopen(argv[1], "rb");
