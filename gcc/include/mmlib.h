70805290134c7d4b3f7c42b17a05197f67e2df09
James T. Sprinkle <the-grue@hotmail.com>
2020-01-29 17:24:12 -0800

    Added some additional MMURTL system calls to the library in support of Standard C Library.

--- gcc/include/mmlib.h
+++ gcc/include/mmlib.h
@@ -6,10 +6,24 @@
 #ifndef __MMLIB_H
 #define __MMLIB_H
 
+int CloseFile(int dHandle);
 int ClrScr();
+int DeleteFile(long dHandle);
 int GetCmdLine(char *pCmdLineRet, int *pdcbCmdLineRet);
+int GetFileLFA(int dHandle, int *pdLFARet);
+int GetFileSize(int dHandle, int *pdSizeRet);
+int SetFileLFA(int dHandle, int dNewLFA);
+int SetFileSize(int dHandle, int dSize);
 int SetJobName(char *pname, int dcbName);
 int Sleep(int dnTicks);
 int ExitJob(int dExitError);
+
+/*
+ *	WriteBytes	250
+ *	ReadBytes	248
+ *	OpenFile	228
+ *	RenameFile	278
+ *	CreateFile	270
+ */
 
 #endif
--- gcc/lib/mmlib.c
+++ gcc/lib/mmlib.c
@@ -3,11 +3,31 @@
  *  
 */
 
+int CloseFile(int dHandle)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "lcall $0x230, $0x00000000;"
+	    : "=a" (rc) : "b" (dHandle) );
+
+	return rc;
+}
+
 int ClrScr()
 {
 	int rc = 0;
 	asm volatile ("lcall $0x1F0, $0x00000000;"
 	    : "=a" (rc) : );
+
+	return rc;
+}
+
+int DeleteFile(int dHandle)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "lcall $0x280, $0x00000000;"
+	    : "=a" (rc) : "b" (dHandle) );
 
 	return rc;
 }
@@ -19,6 +39,50 @@ int GetCmdLine(char *pCmdLineRet, int *pdcbCmdLineRet)
 	    "pushl %%ecx;"
 	    "lcall $0x2D8, $0x00000000;"
 	    : "=a" (rc) : "c" (pdcbCmdLineRet), "b" (pCmdLineRet) ); 
+
+	return rc;
+}
+
+int GetFileLFA(int dHandle, int *pdLFARet)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "pushl %%ecx;"
+	    "lcall $0x258, $0x00000000;"
+	    : "=a" (rc) : "c" (pdLFARet), "b" (dHandle) ); 
+
+	return rc;
+}
+
+int GetFileSize(int dHandle, int *pdSizeRet)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "pushl %%ecx;"
+	    "lcall $0x268, $0x00000000;"
+	    : "=a" (rc) : "c" (pdSizeRet), "b" (dHandle) ); 
+
+	return rc;
+}
+
+int SetFileLFA(int dHandle, int dNewLFA)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "pushl %%ecx;"
+	    "lcall $0x260, $0x00000000;"
+	    : "=a" (rc) : "c" (dNewLFA), "b" (dHandle) ); 
+
+	return rc;
+}
+
+int SetFileSize(int dHandle, int dSize)
+{
+	int rc = 0;
+	asm volatile ("pushl %%ebx;"
+	    "pushl %%ecx;"
+	    "lcall $0x348, $0x00000000;"
+	    : "=a" (rc) : "c" (dSize), "b" (dHandle) ); 
 
 	return rc;
 }
